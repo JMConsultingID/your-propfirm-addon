d234a8ae0f198adf4de1b3926de6f20016008b94
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-28 15:01:53 +0800

    Update class-fyfx-propfirm-user-functions-contact-form.php

--- includes/class-fyfx-propfirm-user-functions-contact-form.php
+++ includes/class-fyfx-propfirm-user-functions-contact-form.php
@@ -23,10 +23,27 @@ function fyfx_your_propfirm_plugin_contact_form_settings_fields() {
             'sanitize_callback' => 'sanitize_text_field'
         )
     );
-
-
 }
 add_action('admin_init', 'fyfx_your_propfirm_plugin_contact_form_settings_fields');
+
+function get_divi_contact_forms() {
+    $args = array(
+        'post_type' => 'page', // Query for pages
+        'posts_per_page' => -1, // Get all pages
+    );
+    $query = new WP_Query($args);
+    $forms = array();
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $forms[get_the_ID()] = get_the_title();
+        }
+        wp_reset_postdata();
+    } else {
+        $forms[] = "None";
+    }
+    return $forms;
+}
 
 // Render general settings section callback
 function fyfx_your_propfirm_plugin_contact_form_section_callback() {
@@ -40,6 +57,23 @@ function fyfx_your_propfirm_plugin_contact_form_enabled_callback() {
     <select name="fyfx_your_propfirm_plugin_contact_form_enabled">
         <option value="enable" <?php selected($plugin_enabled, 'enable'); ?>>Enabled</option>
         <option value="disable" <?php selected($plugin_enabled, 'disable'); ?>>Disabled</option>
+    </select>
+    <?php
+}
+
+function fyfx_your_propfirm_plugin_form_divi_field_callback() {
+    $options = get_option('fyfx_your_propfirm_plugin_form_divi_field');
+    $selected_form_divi = isset($options['fyfx_your_propfirm_plugin_form_divi_field']) ? sanitize_text_field($options['fyfx_your_propfirm_plugin_form_divi_field']) : '';
+    $divi_forms = get_divi_contact_forms();
+    ?>
+    <select name="fyfx_your_propfirm_plugin_form_divi_field">
+        <?php
+            foreach ($divi_forms as $id => $title) {
+                $page_id = $id;
+                $selected = $page_id === $selected_form_divi ? 'selected' : '';
+                echo '<option value="' . $page_id . ' $selected ">' . $title . '</option>';
+            }
+        ?>
     </select>
     <?php
 }
