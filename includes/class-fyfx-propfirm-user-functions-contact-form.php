0ba074e07a894d2f58eb8ccfbb554dbeb9d85ee3
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-28 14:45:47 +0800

    Update class-fyfx-propfirm-user-functions-contact-form.php

--- includes/class-fyfx-propfirm-user-functions-contact-form.php
+++ includes/class-fyfx-propfirm-user-functions-contact-form.php
@@ -1,1 +1,34 @@
 <?php
+// Add plugin settings fields
+function fyfx_your_propfirm_plugin_contact_form_settings_fields() {
+    add_settings_section(
+        'fyfx_your_propfirm_plugin_contact_form_general',
+        'Contact Form Settings',
+        'fyfx_your_propfirm_plugin_contact_form_section_callback',
+        'fyfx_your_propfirm_plugin_contact_form_settings'
+    );
+
+    add_settings_field(
+        'fyfx_your_propfirm_plugin_contact_form_enabled',
+        'Enable YPF Contact Form',
+        'fyfx_your_propfirm_plugin_contact_form_enabled_callback',
+        'fyfx_your_propfirm_plugin_contact_form_settings',
+        'fyfx_your_propfirm_plugin_contact_form_general'
+    );
+
+    register_setting(
+        'fyfx_your_propfirm_plugin_contact_form_settings',
+        'fyfx_your_propfirm_plugin_contact_form_enabled',
+        array(
+            'sanitize_callback' => 'sanitize_text_field'
+        )
+    );
+
+
+}
+add_action('admin_init', 'fyfx_your_propfirm_plugin_contact_form_settings_fields');
+
+// Render general settings section callback
+function fyfx_your_propfirm_plugin_contact_form_section_callback() {
+    echo 'Configure the Contact Form settings for YPF Plugin.';
+}
